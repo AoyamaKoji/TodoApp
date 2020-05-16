12e3ddcda08b8c992a98b95053d926a2079fcbbd
github NamiKikuchi <github k.n.luvivi@gmail.com>
2020-05-17 03:21:01 +0900

    項目表示ページと項目追加ページを作成

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,31 +4,46 @@ namespace App\Http\Controllers;
 
 use App\Category;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+/*DBを利用*/
+use Illuminate\Support\Facades\DB;
 
 class CategoryController extends Controller
 {
 	/*Topページ*/
     public function index(Request $request)
     {
-		$request;
+		/*DB内にあるフィールド'name'を取得　表示*/
+		$items = Category::get();
+		return view('category.index', ['items' => $items]);
 
     }
 
 	/*項目追加*/
     public function add(Request $request)
     {
+		$items = Category::get();
+		return view('category.add', ['items' => $items]);
 
     }
 	public function create(Request $request){
-		
+		$this->validate($request, Category::$rules);
+		$category = new Category;
+		$form = $request->all();
+		unset($form['_token']);
+		$category->fill($form)->save();
+		return redirect('/category');
 	}
 	
 	/*項目削除*/
 	public function update(Request $request){
-		
+		$category = Category::find($request->id);
+		return view('',[]);
 	}
 	public function remove(Request $request){
-		
+		Category::find($request->id)->delete();
+		return redirect('/category');
 	}
 
 }
--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -3,20 +3,62 @@
 @section('title', 'category.index')
 @section('menubar')
   @parent
-	カテゴリー・ページ
+	Top Page
 @endsection
 
 @section('content')
    <table>
-   <tr><th>Data</th></tr>
+   <tr><th>項目一覧</th></tr>
    @foreach ($items as $item)
-      <tr>
-           <td>{{$item->getData()}}</td>
-      </tr>
+   
+    <tr>
+		<td><input type="button" value="{{$item->name}}" onClick="#"></td>
+    </tr>
    @endforeach
    </table>
+   <p>
+   <input type="button" name="add" value="項目追加" onClick="location.href='/category/add'" >
+   
+   </p>
+{{--
+   
+   <p>ソートして表示</p>
+   
+   <input type="button" name="today" value="今日" onClick="">
+   <input type="button" name="week" value="一週間" onClick="">
+   
+   <p>項目変更欄</p>
+   <input type="button" name="add" value="項目追加" onClick="">
+   <input type="button" name="edit" value="項目の名前変更" onClick="">
+   <input type="button" name="del" value="項目の削除" onClick="">
+   
+   @if(isset($_POST['today']))
+	   <form action="/category/today" method="post">
+	   
+   @elseif(isset($_POST['week']))
+		<form action="/category/week" method="post">
+   
+   @elseif(isset($_POST['add']))
+		@csrf
+		<form action="/category/add" method="post">
+		<tr><th>CategoryName: </th><td><input type="text" name="create" value="{{$form->name}}"</td>
+   
+   @elseif(isset($_POST['edit']))
+		<form action="/category/edit" method="post">
+   
+   @else
+		<form action="/category/hoge" method="post">
+	   
+   @endif
+--}}
+  
+   
+   
+   
+   </form>
+   
 @endsection
 
 @section('footer')
-copyright 2020 tuyano.
+copyright 2020 GroupA.
 @endsection
