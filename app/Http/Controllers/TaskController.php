9187e79bf50cbeee1f3a7509bffd4b24c533a3d6
sakorisi <rt.232931372357@gmail.com>
2020-05-18 09:55:32 +0900

    Task周りのControllerとbladeの一新

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,14 +7,25 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
+    //ソートした表を表示
     public function index(Request $request)
     {
+        /* ここにスコープ利用のコード */
         
+        $sort = $request->sort;
+        $items = Task::orderBy($sort, 'asc');
+        $param = ['items' => $items, 'sort' = $sort];
+        return view('task.index', ['items' => $items]);
     }
 
+    
     public function create(Request $request)
     {
-        
+        $this->validate($request, Task::$rules);
+        $task = new Task;
+        unset($form['_token']);
+        $task->fill($form)->save();
+        return redirect('/task');
     }
     
     public function edit(Request $request)
@@ -24,12 +35,19 @@ class TaskController extends Controller
     
     public function update(Request $request)
     {
-        
+        $this->validate($request, Task::$rules);
+        $task = Task::find($request->id);
+        $form = $request->all();
+        unset($form['_token']);
+        $task->fill($form)->save();
+        return redirect('/task');
     }
     
+    //チェックしたものを一斉削除
     public function remove(Request $request)
     {
-        
+        //completeは配列
+        $destroy = Task::destroy(Input::get('complete'));
+        return redirect('/task');
     }
-    
 }
---
+++ resources/views/Task/edit.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.helloapp')
+
+@section('title', 'Task.Add')
+@section('menubar')
+  @parent
+  タスク編集ページ
+@endsection
+
+@section('content')
+    @if (count($errors) > 0)
+    <div>
+        <ul>
+            @foreach ($error->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
+    <form action="/task/edit" method="post">
+    <table>
+        @csrf
+        <input type="hidden" name="id" value="{{$form->id}}">
+        <tr><th>deadline: </th><td><input type="timestamp" name="deadline" value="{{$form->name}}"</td></tr>
+        <tr><th>title: </th><td><input type="text" name="title" value="{{$form->title}}"</td></tr>
+        <tr><th>message: </th><td><input type="text" name="message" value="{{$form->message}}"</td></tr>
+        <tr><th>priority_number: </th><td><input type="number" name="priority_number" value="{{$form->priority_number}}"</td></tr>
+        <tr><th></th><td><input type="submit" value="send"></td></tr>
+    </table>
+    </form>
+@endsection
+
+@section('footer')
+copyright 2020 tuyano.
+@endsection
