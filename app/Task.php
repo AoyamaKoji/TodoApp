5e13a7ab0e161444ab0c383ec7245eda9ba350e7
AoyamaKoji <65073048+AoyamaKoji@users.noreply.github.com>
2020-05-19 15:30:10 +0900

    Update Task.php

--- app/Task.php
+++ app/Task.php
@@ -23,6 +23,15 @@ class Task extends Model
       }
 
       // 指定したカテゴリを出力
+      /**
+      * scopeCategoryIDの説明
+      * @param string $query 第一引数
+      * whereで取得される値と同じ
+      *
+      * @param integer $category_id 第二引数
+      * カテゴリ番号の値
+      *
+      */
       public function scopeCategoryIdEqual($query, $category_id)
       {
 
@@ -30,6 +39,15 @@ class Task extends Model
       }
 
       // 指定した期限のものを出力
+      /**
+      * scopeTimeSpanの説明
+      * @param string $query 第一引数
+      * 
+      * @param datetime $time 第二引数
+      * 指定の時間 etc(20:00:00)
+      * 
+      *
+      */
       public function scopeTimeSpan($query, $time)
       {
           // 期限―今で24時間以内のものを指定する
